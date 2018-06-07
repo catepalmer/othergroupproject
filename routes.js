6937d773289015087bae05f7cd4d8ab8ef9a4f22
anton_ross <dorpetz@hotmail.com>
2018-06-08 11:44:19 +1200

    routes and functions written version1

--- routes.js
+++ routes.js
@@ -2,13 +2,13 @@ const express = require('express')
 const fileFun = require('./fileFun')
 const router = express.Router()
 
-let score = {
-  anton: 0,
-  brad: 0,
-  cate: 0,
-  rebecca: 0,
-  ross: 0
-}
+let score = [
+  {name: "anton", score: 0},
+  {name: "brad", score: 0},
+  {name: "cate", score: 0},
+  {name: "rebecca", score: 0},
+  {name: "ross", score: 0}
+]
 
 router.get('/', (req, res) => {
   res.redirect('/home')
@@ -16,18 +16,25 @@ router.get('/', (req, res) => {
 
 router.get('/home', (req, res) => {
   //make questions = questions.json
-  res.render('home')
+  fileFun.readJSON('./questions.json', obj => {
+    questions = obj
+    res.render('home')
+  })
 })
 
 router.get('/quiz', (req, res) => {
   // question = next question from array
+  let question = questions.pop()
   res.render('quiz', question)
 })
 
 router.get('/profile', (req, res) => {
   //get profiles from profiles.json
-  //winner = profile from the highest score
-  res.render('profile', winner)
+  fileFun.readJSON('./profiles.json', obj => {
+    //winner = profile from the highest score
+    let winner = getWinner(obj, winnerName)
+    res.render('profile', winner)
+  })
 })
 
 router.post('/home', (req, res) => {
@@ -35,12 +42,31 @@ router.post('/home', (req, res) => {
 })
 
 router.post('/quiz', (req, res) => {
-  //if all questions answered
-  res.redirect('profile')
-  //else
+  let answer = req.body.value.toLowerCase()
   // add answer to score
-  res.redirect('quiz')
-
+  addScore(answer)
+  //if all questions answered
+  if (questions.length < 1){
+    res.redirect('profile')
+  }
+  else {
+    res.redirect('quiz')
+  }
 })
+
+function getWinnerName() {
+  score.sort((a, b) => b.score - a.score)
+  return score[0].name // = highest value from score
+}
+
+function getWinner(profiles) {
+  let name = getWinnerName()
+  return profiles.find(obj => obj.name.toLowerCase() == name )
+}
+
+function addScore(answer) {
+  let scorer = score.find(obj => obj.name == answer)
+  scorer.score++
+}
 
 module.exports = router
